7e7fbe08328e0e1cf28f5ef0d61a8e9cc8709dca
yagawa-tcp <69143961+yagawa-tcp@users.noreply.github.com>
2020-09-17 16:09:53 +0900

    データベース作成

--- db/migrate/20200916074802_create_rimit_credits.rb
+++ db/migrate/20200916074802_create_rimit_credits.rb
@@ -1,7 +1,9 @@
 class CreateRimitCredits < ActiveRecord::Migration[6.0]
   def change
     create_table :rimit_credits do |t|
-
+      t.integer   :credit_num,        null: false
+      t.integer   :rimit_consumption
+      t.integer   :alert_consumption
       t.timestamps
     end
   end
