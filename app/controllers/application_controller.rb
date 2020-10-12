10fdd7da2808dbd4f095db5d9551b6163c805810
yagawa-tcp <69143961+yagawa-tcp@users.noreply.github.com>
2020-10-13 01:48:49 +0900

    ユーザーのeditページを機能するように変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, :configure_permitted_parameters2, if: :devise_controller?
 
   # private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :name])
+  end
+  def configure_permitted_parameters2
+    devise_parameter_sanitizer.permit(:account_update, keys: [:limit, :alert])
   end
 end
